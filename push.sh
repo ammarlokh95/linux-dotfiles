700710b852ca36526eb395090545137f4d519c07
ammarlokh95 <ammar.lokh1234@gmail.com>
2024-07-18 05:52:51 +0000

    commiting all changes for the day

--- push.sh
+++ push.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/zsh
+$WD = cwd() 
+cd ./nvim/.config/nvim
+git commit -am "commiting all changes for the day"
+git push 
+
+cd $WD 
+git commit -am "commiting all changes for the day"
 git push --recurse-submodules=on-demand
