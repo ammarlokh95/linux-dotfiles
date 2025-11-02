30beb0a5fbf718872fe4cca0e248273273156db1
Ammar <ammar.lokh1234@gmail.com>
2025-11-02 10:08:58 -0500

    Rearrage all dotfiles

---
+++ .config/.config/waybar/launch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#                    __
+#  _    _____ ___ __/ /  ___ _____
+# | |/|/ / _ `/ // / _ \/ _ `/ __/
+# |__,__/\_,_/\_, /_.__/\_,_/_/
+#            /___/
+#
+# -----------------------------------------------------
+# Quit all running waybar instances
+# -----------------------------------------------------
+killall waybar
+pkill waybar
+sleep 0.5
+
+# -----------------------------------------------------
+# Default theme: /THEMEFOLDER;/VARIATION
+# -----------------------------------------------------
+themestyle="/ml4w-modern;/ml4w-modern/light"
+
+IFS=';' read -ra arrThemes <<<"$themestyle"
+echo ":: Theme: ${arrThemes[0]}"
+
+if [ ! -f ~/.config/waybar/themes${arrThemes[1]}/style.css ]; then
+    themestyle="/ml4w;/ml4w/light"
+fi
+
+# -----------------------------------------------------
+# Loading the configuration
+# -----------------------------------------------------
+config_file="config"
+style_file="style.css"
+
+# Standard files can be overwritten with an existing config-custom or style-custom.css
+if [ -f ~/.config/waybar/themes${arrThemes[0]}/config-custom ]; then
+    config_file="config-custom"
+fi
+if [ -f ~/.config/waybar/themes${arrThemes[1]}/style-custom.css ]; then
+    style_file="style-custom.css"
+fi
+
+waybar -c ~/.config/waybar/themes${arrThemes[0]}/$config_file -s ~/.config/waybar/themes${arrThemes[1]}/$style_file &
